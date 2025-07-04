2d8ca5ade756459f743ba434798cb672d46002d5
Omar Goda <80228400+omargoda99@users.noreply.github.com>
2025-07-04 15:03:09 +0300

    Update Main.java
    delete the name of customer while making an Object reference variable customer due its unnecessary and time consuming and doesn't apply the logic of that real life system

--- FawryTask/Main.java
+++ FawryTask/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Product biscuits = new FreshProduct("Biscuits", 150, 3, LocalDate.now().plusDays(5), 700);
         Product tv = new PhysicalProduct("TV", 300, 5, 180);
         Product scratchCard = new DigitalProduct("Scratch Card", 50, 20);
-        Customer customer = new Customer("Omar", 1000);
+        Customer customer = new Customer(1000);
         Cart cart = new Cart();
         cart.addItem(cheese, 2);
         cart.addItem(biscuits, 1);
