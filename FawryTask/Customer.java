c074fc1dab516ec3beabc8acc9006c7f410c418a
Omar Goda <80228400+omargoda99@users.noreply.github.com>
2025-07-04 15:00:32 +0300

    Update Customer.java
    the process of getting customer name is unnecessary and time consuming

--- FawryTask/Customer.java
+++ FawryTask/Customer.java
@@ -4,21 +4,16 @@
  * @author Omar Goda
  */
 public class Customer {
-    private final String name;
     private double balance;
     
-    public Customer(String name, double balance){
+    public Customer(double balance){
         this.balance=balance;
-        this.name=name;
     }
     public boolean canPay(double moneyAmount){
         return balance >= moneyAmount;
     }
     public void pay(double moneyAmount){
         balance -= moneyAmount;
-    }
-    public String getName(){
-        return name;
     }
     public double getAmount(){
         return balance;
